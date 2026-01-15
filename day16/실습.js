c2db0894e5369f7b99075f0071713651eeacb498
dokyung253-lang <dokyung253@gmail.com>
2026-01-15 16:17:50 +0900

    260115(3)

--- day16/실습.js
+++
@@ -1,8 +0,0 @@
-function boardWrite(){
-    //1. 입력마크업 객체 가져오기
-    const titleInput= document.querySelector('#titleInput');
-    const contentInput= document.querySelector('#contentInput');
-    const pwdInput= document.querySelector('#pwdInput');
-    //2. 입력마크업 객체내 입력값 가져오기
-    const title = titleInput.value
-}
